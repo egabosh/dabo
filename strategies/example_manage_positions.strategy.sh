dc719fc0690887940f4299f7ac28a931997ce00c
olli <>
2024-12-18 12:12:37 +0100

    license info

--- strategies/example_manage_positions.strategy.sh
+++ strategies/example_manage_positions.strategy.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+# Copyright (c) 2022-2024 olli
+#
+# This file is part of dabo (crypto bot).
+# 
+# dabo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dabo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with dabo. If not, see <http://www.gnu.org/licenses/>.
+
 # Example strategy for managing open positions
 
 g_echo_note "EXAMPLE Strategy for managing open positions"
