38f0e6925b57cf0e69a0bf46549199a652c5791e
egabosh <>
2025-05-28 23:09:08 +0200

    complete rewrite

--- dabo/functions/get_range.sh
+++ dabo/functions/get_range.sh
@@ -18,72 +18,198 @@
 # along with dabo. If not, see <http://www.gnu.org/licenses/>.
 
 
+function get_range_all {
+
+  g_echo_note "RUNNING FUNCTION ${FUNCNAME} $@"
+
+  local f_timeframe=$1
+  local f_histfile f_symbol f_symbol_in_array
+
+  get_symbols_ticker
+  for f_symbol in "${f_symbols_array_trade[@]}"
+  do
+    f_symbol=${f_symbol%%:*}
+    f_symbol=${f_symbol//\/}
+
+    # get current price to reduce the range, save cpu-power and time
+    f_symbol_in_array=${f_symbol/ /}
+
+    f_histfile="asset-histories/${f_symbol}.history.$f_timeframe.csv"
+    printf '%(%Y-%m-%d %H:%M:%S)T' >"${f_histfile}.range-calculating"
+    g_echo_note "Estimating current trading range of $f_histfile"
+
+    if get_range "$f_histfile"
+    then
+      printf '%(%Y-%m-%d %H:%M:%S)T' >"${f_histfile}.range-calculated"
+    fi
+    rm -f "${f_histfile}.range-calculating"
+
+  done
+}
+
 
 function get_range {
 
-  #g_echo_note "RUNNING FUNCTION ${FUNCNAME} $@"
+  g_echo_note "RUNNING FUNCTION ${FUNCNAME} $@"
 
-  # get histfile
   local f_hist_file="$1"
-   
-  # Get last days (past day is the usual range for calculationg Levels / Pivot Point) - Should be Tiomezone where most traders on exchange are in
-  local f_latest_date=$(tail -n1 ${f_hist_file} | cut -d, -f1)
-  local f_range_yesterday=$(date "+%F" --date="${f_latest_date} 2-days-ago")
-  local f_range_day=$(date "+%F" --date="${f_latest_date} 3-days-ago")
-  # winter time
-  if date +%Z | grep -q "CET"
-  then
-    local f_range_data=$(egrep "^${f_range_day} [0-2][1-9]:|^${f_range_day} 10:|^${f_range_day} 20:|^${f_range_yesterday} 00:" ${f_hist_file} | cut -d, -f2 | grep "^[0-9]")
-  # summer time
-  elif date +%Z | grep -q "CEST"
-  then
-    local f_range_data=$(egrep "^${f_range_day} [0-2][2-9]:|^${f_range_day} 1[0-1]:|^${f_range_day} 2[0-1]:|^${f_range_today} 0[0-1]:" ${f_hist_file} | cut -d, -f2 | grep "^[0-9]")
-  # TODO: other - timezones!?
-  else
-    local f_range_data=$(grep "^${f_range_day} " ${f_hist_file} | cut -d, -f2 | grep "^[0-9]")
-  fi
+  local f_min_signals=2   # Minimum number of additional signals that must be true (X)
+  local f_min_range_size=5  # Minimum range size in %
 
-  # Check for new range
-  local f_last_range_day=$(tail -n2 ${f_hist_file} | head -n1 | cut -d, -f24)
-  if [[ -n "${f_last_range_day}" ]] 
-  then
-    if echo "${f_range_day}" | grep -q "${f_last_range_day}"
+  local f_last_range_high f_last_range_low f_last_candle_size f_threshold f_abs_move f_candle_change
+
+  # Variables for indicator values
+  local f_last_ema50 f_last_ema200 f_last_rsi14 f_last_volume f_macd_histogram_signal f_last_range_close
+
+  while IFS=, read -r f_date f_open f_high f_low f_close f_volume f_change f_ath f_ema12 f_ema26 f_ema50 f_ema100 f_ema200 f_ema400 f_ema800 f_rsi5 f_rsi14 f_rsi21 f_macd f_macd_ema9_signal f_macd_histogram f_macd_histogram_signal f_macd_histogram_max f_macd_histogram_strength
+  do
+
+    if [[ "$f_hist_file" != *".1w.csv" && "$f_hist_file" != *".1d.csv" ]]
     then
-      #g_echo_note "${FUNCNAME} $@: No new range"
-      return 1
+      if g_num_is_higher $f_open $f_close 
+      then
+        f_high=$f_open
+        f_low=$f_close
+      else
+        f_low=$f_open
+        f_high=$f_close
+      fi
     fi
-  fi
-  
-  if [[ -n "${f_range_data}" ]] 
-  then
-    local f_orig_ifs=${IFS}
-    IFS=\n
-    # get highest, lowest and closing price in range
-    local f_highest_in_range=$(echo ${f_range_data} | sort -n | tail -n1)
-    local f_lowest_in_range=$(echo ${f_range_data} | sort -n | head -n1)
-    local f_closing_price_in_range=$(echo ${f_range_data} | tail -n1)
-    IFS=${f_orig_ifs}
-  
-    # calculate Pivot Point (PP)
-    local f_pivot_point=$(echo "scale=8; (${f_highest_in_range}+${f_lowest_in_range}+${f_closing_price_in_range})/3" | bc | sed 's/^\./0./;')
-  
-    # calculate support/resist and golden pocket
-    local f_support1=$(echo "scale=8; ${f_pivot_point}-((${f_highest_in_range}-${f_lowest_in_range})*0.382)" | bc | sed 's/^\./0./;' )
-    local f_resist1=$(echo "scale=8; ${f_pivot_point}+((${f_highest_in_range}-${f_lowest_in_range})*0.382)" | bc | sed 's/^\./0./;' )
-    local f_golden_pocket_support=$(echo "scale=8; ${f_pivot_point}-((${f_highest_in_range}-${f_lowest_in_range})*0.618)" | bc | sed 's/^\./0./;' )
-    local f_golden_pocket_resist=$(echo "scale=8; ${f_pivot_point}+((${f_highest_in_range}-${f_lowest_in_range})*0.618)" | bc | sed 's/^\./0./;' )
-    local f_golden_pocket65_support=$(echo "scale=8; ${f_pivot_point}-((${f_highest_in_range}-${f_lowest_in_range})*0.65)" | bc | sed 's/^\./0./;' )
-    local f_golden_pocket65_resist=$(echo "scale=8; ${f_pivot_point}+((${f_highest_in_range}-${f_lowest_in_range})*0.65)" | bc | sed 's/^\./0./;' )
-    local f_support3=$(echo "scale=8; ${f_pivot_point}-((${f_highest_in_range}-${f_lowest_in_range})*1)" | bc | sed 's/^\./0./;' )
-    local f_resist3=$(echo "scale=8; ${f_pivot_point}+((${f_highest_in_range}-${f_lowest_in_range})*1)" | bc | sed 's/^\./0./;' )
-  else
-    g_echo_note "${FUNCNAME} $@: Not enough data for calculating range"
-    return 1
-  fi 
-  
-   # write down in history 
-  echo -n ",${f_range_day},${f_lowest_in_range},${f_highest_in_range},${f_pivot_point},${f_support1},${f_resist1},${f_golden_pocket_support},${f_golden_pocket_resist},${f_golden_pocket65_support},${f_golden_pocket65_resist},${f_support3},${f_resist3}" >>"${f_hist_file}"
-  return 0
 
+    # Initialize range and indicators on first line
+    unset f_first
+    if [[ -z "$f_last_range_high" ]]
+    then
+      local f_first=1
+      f_last_range_high="$f_high"
+      f_last_range_low="$f_low"
+    fi
+
+    # Extend range if necessary
+    g_num_is_higher $f_high $f_last_range_high && f_last_range_high="$f_high"
+    g_num_is_lower $f_low $f_last_range_low && f_last_range_low="$f_low"
+
+    # calc range size
+    g_percentage-diff $f_last_range_low $f_last_range_high
+    f_range_size_percentage=$g_percentage_diff_result
+
+    g_calc "$f_high - $f_low"
+    f_candle_change=${g_calc_result#-}
+
+    # calculate threshold for significant move (2/3 change in candle)
+    g_calc "($f_candle_change * 2) / 3"
+    f_threshold=$g_calc_result
+
+    # check if range is large enough: f_min_range_size not first move and only start new range if move is big enough
+    if g_num_is_higher $f_range_size_percentage $f_min_range_size && [[ -z "$f_first" ]] && g_num_is_higher $f_candle_change $f_threshold
+    then
+
+      # Check indicators for significant change
+      local f_signals=0
+      local f_signals_text=""
+
+      # EMA50 cross
+      if g_num_is_higher $f_close $f_ema50 && g_num_is_lower $f_last_range_close $f_last_ema50
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bullish EMA50 Cross"
+      elif g_num_is_lower $f_close $f_ema50 && g_num_is_higher $f_last_range_close $f_last_ema50
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bearish EMA50 Cross"
+      fi
+
+      # EMA50/EMA100 cross
+      if g_num_is_higher $f_ema50 $f_ema100 && g_num_is_lower $f_last_ema50 $f_last_ema100
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bullish EMA50/EMA100 Cross"
+      elif g_num_is_lower $f_ema50 $f_ema100 && g_num_is_higher $f_last_ema50 $f_last_ema100
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bearish EMA50/EMA100 Cross"
+      fi
+
+      # EMA50/EMA200 cross
+      if g_num_is_higher $f_ema50 $f_ema200 && g_num_is_lower $f_last_ema50 $f_last_ema200
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bullish EMA50/EMA200 Cross"
+      elif g_num_is_lower $f_ema50 $f_ema200 && g_num_is_higher $f_last_ema50 $f_last_ema200
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bearish EMA50/EMA200 Cross"
+      fi
+
+      # EMA100/EMA200 cross
+      if g_num_is_higher $f_ema100 $f_ema200 && g_num_is_lower $f_last_ema100 $f_last_ema200
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bullish EMA100/EMA200 Cross"
+      elif g_num_is_lower $f_ema100 $f_ema200 && g_num_is_higher $f_last_ema100 $f_last_ema200
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bearish EMA100/EMA200 Cross"
+      fi
+
+      # MACD cross
+      if [[ $f_macd_histogram_signal = buy ]] || [[ $f_last_macd_histogram_signal = buy ]]
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bullish MACD Cross"
+      elif [[ $f_macd_histogram_signal = sell ]] || [[ $f_last_macd_histogram_signal = sell ]]
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Bearish MACD Cross"
+      fi
+      
+      # RSI overbought/oversold
+      if g_num_is_higher $f_rsi14 70 && g_num_is_lower $f_last_rsi14 70
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" RSI Overbought"
+      elif g_num_is_lower $f_rsi14 30 && g_num_is_higher $f_last_rsi14 30
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" RSI Oversold"
+      fi
+
+      # Volume spike
+      g_calc "$f_last_volume * 1.2"
+      f_volume_spike_threshold_min=$g_calc_result
+      g_calc "$f_last_volume * 1.5"
+      f_volume_spike_threshold_max=$g_calc_result
+      if g_num_is_higher $f_volume $f_volume_spike_threshold_min && g_num_is_lower $f_volume $f_volume_spike_threshold_max
+      then
+        f_signals=$((f_signals+1))
+        f_signals_text+=" Volume Spike"
+      fi
+
+      # DEBUG
+      #[[ -n "$f_signals_text" ]] && echo "$f_date $f_signals_text"
+
+      # Only start new range if at least $f_min_signals signals are present
+      if [ $f_signals -ge $f_min_signals ]
+      then
+        g_echo_note "Current range: $f_last_range_low - $f_last_range_high, size: $f_range_size_percentage%"
+        echo "$f_last_range_low $f_last_range_high" >${f_hist_file}.range
+        # DEBUG
+        #echo "  Signals ($f_signals):$f_signals_text"
+        break
+      fi
+      
+    fi
+
+    # Update indicator values for next iteration
+    f_last_range_close=$f_close
+    f_last_ema50=$f_ema50
+    f_last_ema100=$f_ema100
+    f_last_ema200=$f_ema200
+    f_last_rsi14=$f_rsi14
+    f_last_volume=$f_volume
+    f_last_macd_histogram_signal=$f_macd_histogram_signal
+
+  done < <(tac "$f_hist_file")
 }
+
 
