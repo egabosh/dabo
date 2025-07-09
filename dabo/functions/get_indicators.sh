a166720038f7717f96a7b2b3f746f0f12274edf1
egabosh <>
2025-07-09 19:02:55 +0200

    better checks

--- dabo/functions/get_indicators.sh
+++ dabo/functions/get_indicators.sh
@@ -21,7 +21,7 @@
 function get_indicators_all {
 
   g_echo_note "RUNNING FUNCTION ${FUNCNAME} $@"
-  
+ 
   local f_last_intervals="$1"
   
   local f_histfile f_symbol
@@ -77,38 +77,45 @@ function get_indicators_all {
 function get_indicators {
 
   g_echo_note "RUNNING FUNCTION ${FUNCNAME} $@"
+
   local f_histfile="$1"
   local f_last_intervals="$2"
   # max 8928 for large files (~1 month in 5m interval; ~24 years in 1d interval)
   [[ -z $f_last_intervals ]] && f_last_intervals=8928
+  [[ $f_last_intervals -gt 8928 ]] && f_last_intervals=8928
   local f_fill_missing_ohlcv_intervals=$3
   local f_line 
 
   # check if the job is already done
-  if [[ $(wc -l <"${f_histfile}") -gt 801 ]] && [[ $f_last_intervals -gt 801 ]]
+  if ! tail -n $f_last_intervals "${f_histfile}" | egrep -vq "^([0-9]){4}-([0-9]){2}-([0-9]){2}.*,([0-9\.]+,){5}[0-9\.\-]+,([0-9\.]+,){11}([0-9\.\-]+,){3}[a-z]*(,[0-9\.\-]+){2}" 
   then
-    if ! tail -n $f_last_intervals "${f_histfile}" | tail +801 | egrep -vq "^....-..-..*,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.\-]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.]+,[0-9\.\-]+,[0-9\.\-]+,[0-9\.\-]+,[a-z]*,[0-9\.\-]+,[0-9\.\-]+"
-    then
-      g_echo_note "${f_histfile} seems to be indicator-complete"
-      return 0
-    fi
+    g_echo_note "${f_histfile} seems to be indicator-complete for the last ($f_last_intervals) intervals"
+    return 0
   fi
   
   # history
   local f_columns="date,open,high,low,close,volume,change,ath,ema12,ema26,ema50,ema100,ema200,ema400,ema800,rsi5,rsi14,rsi21,macd,macd_ema9_signal,macd_histogram,macd_histogram_signal,macd_histogram_max,macd_histogram_strength"
   local f_emas="12 26 50 100 200 400 800"
   local f_rsis="5 14 21"
-  local f_ema f_change f_changed f_line f_valid_data f_ath f_last_year
+  local f_ema f_change f_changed f_line f_valid_data f_ath f_last_year f_check_var
   local f_columns_space="${f_columns//,/ }"
   g_read_csv "${f_histfile}" "${f_last_intervals}" "$f_columns"
   for ((i=0; i<=${#g_csv_array[@]}-1; i++))
   do
 
-    if [[ -z "${v_csv_array_associative[date_${i}]}" ]] || [[ -z "${v_csv_array_associative[open_${i}]}" ]] 
+    # check vars
+    if ! [[ "${v_csv_array_associative[date_${i}]}" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2} ]]
     then
-      g_echo_note "No data $f_histfile:${v_csv_array_associative[date_${i}]}"
-      return 0
+      g_echo_warn "No valid date $f_histfile:${v_csv_array_associative[date_${i}]} - stopping here"
+      return 1
     fi
+    if ! g_num_valid_number "${v_csv_array_associative[open_${i}]}"
+    then
+      g_echo_warn "No Open data $f_histfile:${v_csv_array_associative[date_${i}]} - stopping here"
+      return 1
+    fi
+
+
     #g_echo_note "=== $0 for $f_histfile:${v_csv_array_associative[date_${i}]},$f_histfile:${v_csv_array_associative[close_${i}]}"
 
     # get previous position
@@ -117,11 +124,11 @@ function get_indicators {
     ### check for unfilled fields
     f_change=""
 
-    # check olhc data
+    # fix olhc data
     if [[ -z "${v_csv_array_associative[high_${i}]}" ]] && [[ -z "${v_csv_array_associative[low_${i}]}" ]] && [[ -z "${v_csv_array_associative[close_${i}]}" ]] 
     then
       g_echo_note "fixing OHLC Data"
-      # open is close
+      # close is open
       v_csv_array_associative[close_${i}]=${v_csv_array_associative[open_${i}]}
       # open is previous close
       [[ -n "${v_csv_array_associative[close_${p}]}" ]]  && v_csv_array_associative[open_${i}]="${v_csv_array_associative[close_${p}]}"
@@ -137,6 +144,13 @@ function get_indicators {
       f_change=1
     fi
     
+    # chexk ohlc data
+    if ! g_num_valid_number "${v_csv_array_associative[open_${i}]}" "${v_csv_array_associative[high_${i}]}" "${v_csv_array_associative[low_${i}]}" "${v_csv_array_associative[close_${i}]}"
+    then
+      g_echo_warn "OHLC data incomplete $f_histfile:${v_csv_array_associative[date_${i}]} - stopping here"
+      return 1
+    fi
+
     # check for missing percentage change
     if [[ -z "${v_csv_array_associative[change_${i}]}" ]] 
     then
@@ -156,17 +170,21 @@ function get_indicators {
     fi
     
     # ath (all-time-high) of present data
-    if [[ -z "${v_csv_array_associative[ath_${p}]}" ]] 
+    if [[ -z "${v_csv_array_associative[ath_${i}]}" ]]
     then
-      # define max for the first time 
-      v_csv_array_associative[ath_${i}]=${v_csv_array_associative[high_${i}]}
-    else
-      if g_num_is_higher ${v_csv_array_associative[high_${i}]} ${v_csv_array_associative[ath_${p}]}
+      if [[ -z "${v_csv_array_associative[ath_${p}]}" ]] 
       then
+        # define max for the first time 
         v_csv_array_associative[ath_${i}]=${v_csv_array_associative[high_${i}]}
       else
-        v_csv_array_associative[ath_${i}]=${v_csv_array_associative[ath_${p}]}
+        if g_num_is_higher ${v_csv_array_associative[high_${i}]} ${v_csv_array_associative[ath_${p}]}
+        then
+          v_csv_array_associative[ath_${i}]=${v_csv_array_associative[high_${i}]}
+        else
+          v_csv_array_associative[ath_${i}]=${v_csv_array_associative[ath_${p}]}
+        fi
       fi
+      f_change=1
     fi
 
     # check for missing EMAs
@@ -230,9 +248,12 @@ function get_indicators {
       IFS=$oldIFS
     fi
 
+
   done
   
   # cleanup large arrays
   unset v vr v_csv_array_associative v_csv_array_associative_reverse 
+
+
 }
 
