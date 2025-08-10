a4f1e0fd567d1c5337e29b991adfc63a5775a37e
egabosh <>
2025-08-10 12:32:06 +0200

    month saisonality indicator/statistic; vars/files _ instead of -; cleanup

--- dabo/functions/get_values.sh
+++ dabo/functions/get_values.sh
@@ -37,17 +37,17 @@ function get_values {
     f_asset_histories+="MARKETDATA_BINANCE_LONG_SHORT_RATIO_ACCOUNT_$f_asset "
   done
 
-  local f_eco_asset f_eco_assets f_asset f_time f_prefix f_histfile f_columns f_return f_levelsfile f_tmp_levels f_first f_lstmfilea f_rangefile
+  local f_eco_asset f_eco_assets f_asset f_time f_prefix f_histfile f_columns f_return f_levelsfile f_tmp_levels f_first f_lstmfilea f_rangefile ldate lside lprice lpercentage lupprice ldownprice lmedian_current_month lmedian_next_month lmedian_current_next_month_average
   
-  for f_eco_asset in $ECO_ASSETS
-  do
-    if [[ -z "$f_eco_assets" ]] 
-    then
-      f_eco_assets="ECONOMY-${f_eco_asset}"
-    else
-      f_eco_assets="$f_eco_assets ECONOMY-${f_eco_asset}"
-    fi
-  done
+  #for f_eco_asset in $ECO_ASSETS
+  #do
+  #  if [[ -z "$f_eco_assets" ]] 
+  #  then
+  #    f_eco_assets="ECONOMY_${f_eco_asset}"
+  #  else
+  #    f_eco_assets="$f_eco_assets ECONOMY_${f_eco_asset}"
+  #  fi
+  #done
   
   # get current prices from exchange
   get_symbols_ticker
@@ -56,26 +56,27 @@ function get_values {
   for f_asset in $f_asset_histories\
    BTC${CURRENCY}\
    $f_eco_assets\
-   MARKETDATA_BINANCE_OPEN_INTEREST_BTC${CURRENCY}\
-   MARKETDATA_BINANCE_LONG_SHORT_RATIO_TAKER_BTC${CURRENCY}\
-   MARKETDATA_BINANCE_LONG_SHORT_RATIO_ACCOUNT_BTC${CURRENCY}\
-   MARKETDATA_FEAR_AND_GREED_ALTERNATIVEME\
-   MARKETDATA_FEAR_AND_GREED_CNN\
-   MARKETDATA_US_CONSUMER_PRICE_INDEX_CPI\
-   MARKETDATA_US_FED_FUNDS_RATE MARKETDATA_US_UNEMPLOYMENT_RATE\
-   MARKETDATA_US_FED_M2_SL_MONEY_SUPPLY\
-   MARKETDATA_US_FED_M2_NS_MONEY_SUPPLY
+   $f_market_data
   do
+   #MARKETDATA_BINANCE_OPEN_INTEREST_BTC${CURRENCY}\
+   #MARKETDATA_BINANCE_LONG_SHORT_RATIO_TAKER_BTC${CURRENCY}\
+   #MARKETDATA_BINANCE_LONG_SHORT_RATIO_ACCOUNT_BTC${CURRENCY}\
+   #MARKETDATA_FEAR_AND_GREED_ALTERNATIVEME\
+   #MARKETDATA_FEAR_AND_GREED_CNN\
+   #MARKETDATA_US_CONSUMER_PRICE_INDEX_CPI\
+   #MARKETDATA_US_FED_FUNDS_RATE MARKETDATA_US_UNEMPLOYMENT_RATE\
+   #MARKETDATA_US_FED_M2_SL_MONEY_SUPPLY\
+   #MARKETDATA_US_FED_M2_NS_MONEY_SUPPLY
 
     # read latest ohlcv data and indicators per timeframe to vars
     for f_time in 5m 15m 1h 4h 1d 1w 1M
     do
       # special on ECONOMY data
       f_prefix="${f_time}_"
-      if [[ "$f_asset" =~ ^ECONOMY- ]] 
+      if [[ "$f_asset" =~ ^ECONOMY_ ]] 
       then
         f_prefix="${f_asset}_${f_time}_"
-        f_prefix=${f_prefix//-/_}
+        #f_prefix=${f_prefix//-/_}
       fi
       
       # histfile
@@ -113,17 +114,28 @@ function get_values {
           f_liquidityfile="asset-histories/${f_asset}.history.1h.liquidity_${f_liquiditytime}.csv"
           if [[ -s "$f_liquidityfile" ]]
           then
-            while IFS=, read ldate lside lprice lpercentage lupprice ldownprice
-            do
-              vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_date]=$ldate
-              vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_side]=$lside
-              vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_price]=$lprice
-              vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_percentage]=$lpercentage
-              vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_upprice]=$lupprice
-              vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_downprice]=$ldownprice
-            done < <(tail -n 1 "$f_liquidityfile")
+            IFS=, read ldate lside lprice lpercentage lupprice ldownprice < <(tail -n 1 "$f_liquidityfile")
+            vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_date]=$ldate
+            vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_side]=$lside
+            vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_price]=$lprice
+            vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_percentage]=$lpercentage
+            vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_upprice]=$lupprice
+            vr[${f_asset}_${f_time}_liquidity_${f_liquiditytime}_downprice]=$ldownprice
           fi
         done
+      fi
+      
+      # check for month saisonality indicator/statistic
+      if [[ $f_time = 1d ]]
+      then
+        if [[ -s asset-histories/${f_asset}.saisonality ]]
+        then
+          IFS=, read ldate lmedian_current_month lmedian_next_month lmedian_current_next_month_average < <(tail -n 1 "asset-histories/${f_asset}.saisonality")
+          vr[${f_asset}_1M_saisonality_date]=$ldate
+          vr[${f_asset}_1M_saisonality_median_current_month]=$lmedian_current_month
+          vr[${f_asset}_1M_saisonality_median_next_month]=$lmedian_next_month
+          vr[${f_asset}_1M_saisonality_median_current_next_month_average]=$lmedian_current_next_month_average
+        fi
       fi
 
     done
@@ -131,31 +143,6 @@ function get_values {
     # read current levels and ai predictions
     for f_time in 1w 1d
     do
-      #f_levelsfile="asset-histories/${f_asset}.history.${f_time}.csv.levels"
-      #if [[ -s "$f_levelsfile" ]]  
-      #then
-      #  # get levels
-      #  read -r -a f_levels <"$f_levelsfile"
-      #  vr[${f_asset}_levels_$f_time]="${f_levels[*]}"
-      #  
-      #  # add current price and sort
-      #  f_levels+=("${vr[${f_asset}_price]}")
-      #  oldIFS="$IFS"
-      #  IFS=$'\n' f_levels_sorted=($(sort -n <<<"${f_levels[*]}"))
-      #  IFS="$oldIFS"
-      #  
-      #  # find current price and +- one for upper lower price
-      #  for ((i=0; i<${#f_levels_sorted[@]}; i++))
-      #  do
-      #    if [[ "${f_levels_sorted[$i]}" = "${vr[${f_asset}_price]}" ]] 
-      #    then
-      #      vr[${f_asset}_levels_${f_time}_next_up]=${f_levels_sorted[i+1]}
-      #      vr[${f_asset}_levels_${f_time}_next_down]=${f_levels_sorted[i-1]}
-      #      break
-      #    fi
-      #  done
-      #fi
-
       f_lstmfile="asset-histories/${f_asset}.history.${f_time}.lstm_prediction.csv"
       if [[ -s "$f_lstmfile" ]]  
       then
@@ -166,7 +153,7 @@ function get_values {
   done
 
   # read m2_3_month_dely from file
-  read -r vr[m2_3_month_delay] < m2_3_month_delay
+  [[ -s "m2_3_month_delay" ]] && read -r vr[m2_3_month_delay] < m2_3_month_delay
 
   # use reverse as default to be 0 latest, 1 pre latest,...
   unset v
